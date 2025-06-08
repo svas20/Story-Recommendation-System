59538c196d9e0c012049279c28ca79e370cd3e6b
Satya Srinivas <satyavas12@gmail.com>
2025-06-08 00:18:15 -0400

    Switch to pyproject.toml and uv.lock, removed old setup files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="AI_Agent_Engineer_Takehome",           # Name of the installable package
-    version="0.1.0",
-    packages=find_packages(),    # Automatically finds 'agent_code'
-    install_requires=[]          # Add any required dependencies here
-)
